1aa72841ca08527c8b88c825b78986a19dce373b
rohit <kumbharrohit13@gmail.com>
2020-07-08 19:49:37 +0530

    @Transactional annotation added on bookFlight() in ReservationServiceImpl.java

--- flightreservation/src/main/java/com/rohit/flightreservation/services/ReservationServiceImpl.java
+++ flightreservation/src/main/java/com/rohit/flightreservation/services/ReservationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.rohit.flightreservation.services;
 
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +45,7 @@ public class ReservationServiceImpl implements ReservationService {
 	// Functionality)
 
 	@Override
+	@Transactional
 	public Reservation bookFlight(ReservationRequest request) {
 		LOGGER.info("Inside bookFlight() in ReservationServiceImpl");
 
