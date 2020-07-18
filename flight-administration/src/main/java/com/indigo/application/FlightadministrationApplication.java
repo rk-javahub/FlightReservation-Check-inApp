a3059f345b490d855a3368b9e30d0d87e7d3643c
rohit <kumbharrohit13@gmail.com>
2020-07-18 11:07:02 +0530

    Refresh commit on 18-7-2020

---
+++ flight-administration/src/main/java/com/indigo/application/FlightadministrationApplication.java
@@ -0,0 +1,13 @@
+package com.indigo.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightadministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightadministrationApplication.class, args);
+
+	}
+}
--- flightreservation/src/main/java/com/rohit/flightreservation/entities/Role.java
+++ flightreservation/src/main/java/com/rohit/flightreservation/entities/Role.java
@@ -1,12 +1,9 @@
 package com.rohit.flightreservation.entities;
 
 import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
-
 import org.springframework.security.core.GrantedAuthority;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,13 +11,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Role extends AbstractEntity implements GrantedAuthority {
+	private static final long serialVersionUID = 1L;
 	private String name;
 	@ManyToMany(mappedBy = "roles")
 	private Set<User> users;
 
 	@Override
 	public String getAuthority() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 }
