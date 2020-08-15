99903d0aa082a74722fabf613f4dd70a971ac3de
rohit <kumbharrohit13@gmail.com>
2020-08-15 19:33:59 +0530

    Test class added

--- flightreservation/src/test/java/com/rohit/flightreservation/FlightreservationApplicationTests.java
+++ flightreservation/src/test/java/com/rohit/flightreservation/FlightreservationApplicationTests.java
@@ -1,19 +1,17 @@
 package com.rohit.flightreservation;
 
-
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = FlightreservationApplication.class)
 public class FlightreservationApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		
+
 	}
 
 }
